4bd43fff0dfff68916084e633dd4a1a6210df8e1
TI01045 <TI01045@BDTUDV31328.bbcl.local>
2018-05-18 15:46:57 +0530

    ZOHO_Care

---
+++ ZOHO/src/AntTestNG/radioButton.java
@@ -0,0 +1,44 @@
+package AntTestNG;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class radioButton {
+
+	public static void main(String[] args) {
+		WebDriver dr = new FirefoxDriver();
+		dr.manage().window().maximize();
+		dr.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		dr.get("http://www.echoecho.com/htmlforms10.htm");
+		System.out.println(dr.getTitle());
+		List<WebElement> radiobtn = dr.findElements(By.name("group1"));
+		System.out.println(radiobtn.size());
+		for(int i=0;i<radiobtn.size();i++)
+		{
+		   System.out.println(radiobtn.get(i).getAttribute("checked"));
+		}
+		radiobtn = dr.findElements(By.name("group2"));
+		for(int i=0;i<radiobtn.size();i++){
+			System.out.println(radiobtn.get(i).getAttribute("checked"));
+		}
+		
+		File srcShot = ((TakesScreenshot)dr).getScreenshotAs(OutputType.FILE);
+		try{
+			 FileUtils.copyFile(srcShot, new File("D://report//sssss.jpg"));
+		   }
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+		
+	}
+
+}
---
+++ ZOHO/src/FirefoxProfilePack/Firefox_ProfileClass.java
@@ -0,0 +1,32 @@
+package FirefoxProfilePack;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+public class Firefox_ProfileClass {
+    static WebDriver driver = null;
+	public static void main(String[] args) throws Exception {
+		ProfilesIni allPro = new ProfilesIni();
+		FirefoxProfile profileFirefox = allPro.getProfile("default_Manoj");
+		driver = new FirefoxDriver(profileFirefox);
+		driver.manage().window().maximize();
+		//driver.get("https://google.com");
+		File src= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		try{
+	    // now copy the  screenshot to desired location using copyFile //method
+		FileUtils.copyFile(src, new File("C:/Users/ti01045/Desktop/BRD-2016/googleScreenShot.jpg"));
+		}
+		catch (Exception e)
+		 {
+		  System.out.println(e.getMessage());
+		 }
+		System.out.println(driver.getTitle());
+	}
+}
